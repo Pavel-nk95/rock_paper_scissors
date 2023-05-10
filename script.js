0c92de5a50a4d1e634b9a9ed49a663c463d7f748
Pavel <pn9-5@yandex.ru>
2023-05-10 16:18:11 +0300

    feat: task is completed

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const items = ['rock', 'paper', 'scissors'];
 
+const buttons = document.querySelector('.items');
+const rockBtn = document.querySelector('.item-rock');
+const paperBtn = document.querySelector('.item-paper');
+const scissorsBtn = document.querySelector('.item-scissors');
+
+const personScore = document.querySelector('.person-score strong');
+const computerScore = document.querySelector('.computer-score strong');
+
+const message = document.querySelector('.message');
+const roundMsg = document.querySelector('.round-msg strong');
+
+let playerCount = 0;
+let computerCount = 0;
+let rounds = 1;
+
 function getComputerChoice() {
   return items[Math.floor(Math.random() * items.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  const playerSelectionNormalized = playerSelection.toLowerCase();
   const resultData = {
     'rock': {
       'paper': false,
@@ -20,39 +34,69 @@ function playRound(playerSelection, computerSelection) {
       'paper': true,
     },
   };
-  switch (resultData[playerSelectionNormalized][computerSelection]) {
+  switch (resultData[playerSelection][computerSelection]) {
     case true:
-      return [1, `You Win! ${playerSelectionNormalized} beats ${computerSelection}`];
+      return [true, `You Win! ${playerSelection} beats ${computerSelection}`];
     case false:
-      return [-1, `You Lose! ${computerSelection} beats ${playerSelectionNormalized}`];
+      return [false, `You Lose! ${computerSelection} beats ${playerSelection}`];
     default:
-      return [0, 'This is dead heat!'];
+      return [null, 'This is dead heat!'];
+  }
+}
+
+function render(key, answer) {
+  if (key === null) message.textContent = answer;
+  if (key) {
+    playerCount += 1;
+    message.textContent = answer;
+    personScore.textContent = playerCount;
+  }
+  if (!key && key !== null) {
+    computerCount += 1;
+    message.textContent = answer;
+    computerScore.textContent = computerCount;
+  }
+  rounds += 1;
+  roundMsg.textContent = rounds;
+}
+
+function isWin() {
+  if (playerCount === 5 || computerCount === 5) {
+    if (playerCount > computerCount) {
+      buttons.replaceChildren();
+      message.textContent = 'Game over! You Win!';
+      return;
+    }
+    if (playerCount < computerCount) {
+      buttons.replaceChildren();
+      message.textContent = 'Game over! You Lose!';
+      return;
+    }
   }
 }
 
 function game() {
-  let count = 0;
-  const rounds = 5;
-  for (let i = 0; i < rounds; i += 1) {
-    const playerSelection = prompt(`Enter your answer! Round: ${i + 1}`);
-    if (playerSelection === null) {
-      console.log('Game over! Bye!');
-      return;
-    }
-    if (!items.includes(playerSelection.toLowerCase())) {
-      console.log('Try again!');
-      i -= 1;
-      continue;
-    }
-    const computerSelection = getComputerChoice();
-    const [updateCount, answer] = playRound(playerSelection, computerSelection);
-    count += updateCount;
-    console.log(answer);
-  }
-  if (count === 0) console.log('Game over! This is dead heat!');
-  if (count > 0) console.log(`Game over! You Win!`);
-  if (count < 0) console.log(`Game over! You Lose!`);
-  return;
+
+  rockBtn.addEventListener('click', () => {
+    const playerSelection = 'rock';
+    const [key, answer] = playRound(playerSelection, getComputerChoice());
+    render(key, answer);
+    isWin();
+  });
+
+  paperBtn.addEventListener('click', () => {
+    const playerSelection = 'paper';
+    const [key, answer] = playRound(playerSelection, getComputerChoice());
+    render(key, answer);
+    isWin();
+  });
+
+  scissorsBtn.addEventListener('click', () => {
+    const playerSelection = 'scissors';
+    const [key, answer] = playRound(playerSelection, getComputerChoice());
+    render(key, answer);
+    isWin();
+  });
 }
 
 game();
